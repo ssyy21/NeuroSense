171ed9700c5a734045c531c253a70b6410e456ef
Sneha Yadav <143681944+ssyy21@users.noreply.github.com>
2025-04-11 10:27:40 +0530

    Update app.py

--- Mentalwellness/app.py
+++ Mentalwellness/app.py
@@ -9,11 +9,7 @@ st.set_page_config(
 import random
 import requests
 from googleapiclient.discovery import build
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("Mentalwellness/styles.css")  # adjust path if needed
 
 
 from dotenv import load_dotenv
